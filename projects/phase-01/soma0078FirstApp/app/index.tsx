204c807b8e2904f6e43c763f0edace990a200804
Songa Lee <104878713+soma0078@users.noreply.github.com>
2026-02-26 06:28:33 -0800

    docs: update tli(RN 기본 컴포넌트)

--- projects/phase-01/soma0078FirstApp/app/index.tsx
+++ projects/phase-01/soma0078FirstApp/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Alert, Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,6 +9,20 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Pressable
+        onPress={() => Alert.alert('pressed!')}
+        style={({pressed})=> [{
+          backgroundColor: pressed ? 'orange' : 'yellow',
+        },
+          {
+            padding: 10,
+            borderRadius: 5,
+            color: 'white',
+          }]
+        }
+      >
+        <Text>Press me</Text>
+      </Pressable>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
